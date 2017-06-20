2b88cf1d8f97361448db0d448bc0d15fbaedda37
Simonas Sankauskas <simonas.sank@gmail.com>
2017-06-20 18:00:20 +0300

    Init

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Rate Me API
+### Written in GO lang using Macaron web framework.
+
+```
+go get gopkg.in/macaron.v1
+go get github.com/go-macaron/pongo2
+go get github.com/jinzhu/gorm
+go get github.com/go-macaron/binding
+go get github.com/go-sql-driver/mysql
+go get github.com/buger/jsonparser
+```
---
+++ src/server.go
@@ -0,0 +1,66 @@
+package main
+
+import(
+  "gopkg.in/macaron.v1"
+  "github.com/go-macaron/pongo2"
+  "github.com/jinzhu/gorm"
+  "github.com/go-macaron/binding"
+  _ "github.com/go-sql-driver/mysql"
+  "github.com/buger/jsonparser"
+  "io/ioutil"
+  "fmt"
+)
+
+type Rating struct {
+  gorm.Model
+  App_package_name    string `json:"app_package_name"`
+  Rating              float32 `json:"rating"`
+  Description         string `json:"description"`
+  Email               string `json:"email"`
+  Device_id           string `json:"device_id"`
+  Ip                  string `json:"ip"`
+}
+
+func main() {
+  config, err := ioutil.ReadFile("./config.json")
+
+  mysql_user, _ := jsonparser.GetString(config, "mysql-user")
+  mysql_pass, _ := jsonparser.GetString(config, "mysql-pass")
+  mysql_database, _ := jsonparser.GetString(config, "mysql-database")
+  server_port, _ := jsonparser.GetInt(config, "server-port")
+
+  if (err != nil) {
+    fmt.Println(err)
+    return
+  }
+
+  // Init Web Server
+  macaron.Env = macaron.PROD
+  m := macaron.Classic()
+  m.Use(macaron.Recovery())
+  m.Use(pongo2.Pongoer())
+
+  // Init Database ORM
+  connection_params := mysql_user + ":" + mysql_pass + "@/" + mysql_database + "?charset=utf8&parseTime=True&loc=Local"
+  db, err := gorm.Open("mysql", connection_params)
+  
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+  // Migrate the schema
+  db.AutoMigrate(&Rating{})
+
+  // Server Status Endpoint
+  m.Get("/", func() string {
+    return "Alive!"
+  })
+  
+  // Rating Insert Endpoint
+  m.Post("/rating", binding.Bind(Rating{}), func(rating Rating) string {
+    db.Create(&rating)
+    return "Ok"
+  })
+
+  m.Run("0.0.0.0", server_port)
+}
